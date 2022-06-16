b8da199d49205ee07cb020970bbec0862d20a9ce
Razmik Karapetyan <karapetyanraz95@gmail.com>
2022-06-16 22:37:40 +0400

    add stdafx to the project

---
+++ stdafx/stdafx.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
